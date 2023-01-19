1f470286fea0974ae4d2152c6fadd10b10e6cd21
alvaro <arecabarren14@alumnos.utalca.cl>
2023-01-19 16:51:48 -0300

    Finish exercise 1

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ head3.textContent = "I'm a blue h3!"
 head3.style.color = 'blue'
 
 var styledDiv = document.createElement('div')
-styledDiv.style['border: 1 px solid black'] //Trying differents syntaxes
+styledDiv.style.border = '1px solid black' //Trying differents syntaxes
 styledDiv.style.backgroundColor = 'pink'
 
+let lyerHeader = document.createElement('h1')
+lyerHeader.textContent = "I'm a div"
 
+let lyerPar = document.createElement('p')
+lyerPar.textContent = "ME TOO!"
+
+styledDiv.appendChild(lyerHeader)
+styledDiv.appendChild(lyerPar)
 
 
 var container = document.querySelector('#container')
 container.appendChild(redParagraph)
 container.appendChild(head3)
+container.appendChild(styledDiv)
 
